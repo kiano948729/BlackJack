9c47824630a7955f9666b1e3a5ee66f5a739d86f
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2026-03-11 10:10:02 +0100

    Voeg Card-model, Rank/Suit-enums en kaartafbeeldingen toe
    basiskaarttypes en assets voor OOPBlackJack: voeg Rank en Suit enums toe en een Card-model (Id, Suit, Rank, Value, ImagePath, IsFaceUp) met een Flip()-methode.

---
+++ Enums/Suit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPBlackJack.Enums
+{
+    public enum Suit
+    {
+        Spades,
+        Hearts,
+        Diamonds,
+        Clubs
+    }
+}
