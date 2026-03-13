18405915e92fd4289f1450093c35380d67df80d0
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2026-03-13 11:46:13 +0100

    Deck-model en UI-kaartacties toegevoegd
    Voegt een Deck-klasse toe (generate, shuffle, draw, cards remaining) en koppelt deze aan de Form1-UI. Toont getrokken kaarten in een PictureBox met foutafhandeling en debuglogging. Voegt UI-knoppen toe.

---
+++ Models/Deck.cs
@@ -0,0 +1,68 @@
+﻿using OOPBlackJack.Enums;
+
+namespace OOPBlackJack.Models
+{
+    public class Deck
+    {
+        private readonly Random random = new();
+        public List<Card> Cards { get; }
+
+        public Deck()
+        {
+            Cards = new List<Card>();
+            GenerateDeck();
+        }
+
+        private void GenerateDeck()
+        {
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    string rankName = rank switch
+                    {
+                        Rank.ACE => "ace",
+                        Rank.JACK => "jack",
+                        Rank.QUEEN => "queen",
+                        Rank.KING => "king",
+                        // omdat enum bij 0 begint (ACE=0)
+                        _ => ((int)rank + 1).ToString() 
+                    };
+
+                    Cards.Add(new Card(
+                        suit,
+                        rank,
+                        $"PNG-cards-1.3/{rankName}_of_{suit.ToString().ToLower()}.png"
+                    ));
+                }
+            }
+        }
+
+        // Fisher-Yates shuffle algoritme
+        public void Shuffle()
+        {
+            for (int i = Cards.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                Card temp = Cards[i];
+                Cards[i] = Cards[j];
+                Cards[j] = temp;
+            }
+        }
+
+        public Card DrawCard()
+        {
+            if (Cards.Count == 0)
+                throw new InvalidOperationException("Geen kaarten over in het deck.");
+
+            Card card = Cards[0];
+            Cards.RemoveAt(0);
+            return card;
+        }
+
+        public int CardsRemaining()
+        {
+            return Cards.Count;
+        }
+    }
+}
